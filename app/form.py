45a686ef5c7dc1f78d2221310b31a41a9162ec7c
liyu-task <878077525@qq.com>
2019-04-04 10:12:12 +0800

    V1.0

---
+++ app/form.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from flask_ckeditor import CKEditorField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class BaseForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 20)],
+                           render_kw={'class': 'form-control', 'placeholder': "请输入用户名"})
+    password = PasswordField('密码', validators=[DataRequired()],
+                             render_kw={'class': 'form-control', 'placeholder': "请确认密码"})
+
+
+class RegisterForm(BaseForm):
+    email = StringField('邮箱', validators=[DataRequired(), Email()],
+                        render_kw={'class': 'form-control', 'placeholder': '请输入邮箱'})
+    confirm_pwd = PasswordField('密码', validators=[DataRequired()],
+                                render_kw={'class': 'form-control', 'placeholder': "请确认密码"})
+    register = SubmitField('注册', render_kw={'class': ['form-control', 'btn'], 'value': '注册'})
+
+
+class LoginForm(BaseForm):
+    login = SubmitField('登陆', render_kw={'class': ['form-control', 'btn'], 'value': '登陆'})
+
+
+class CategoryForm(FlaskForm):
+    category = StringField('分类', validators=[DataRequired()],
+                           render_kw={'class': 'form-control', 'placeholder': '文章分类'})
+
+
+class PostForm(FlaskForm):
+    title = StringField('标题', validators=[DataRequired(), Length(1, 20)],
+                        render_kw={'class': 'form-control', 'placeholder': '请输入文章标题'})
+    body = CKEditorField('正文', validators=[DataRequired()],
+                         render_kw={'class': 'form-control', 'placeholder': '请输入正文'})
+    submit = SubmitField('提交', render_kw={'class': ['form-control', 'btn'], 'value': '提交'})
---
+++ app/views.py
@@ -0,0 +1,193 @@
+from flask import render_template, url_for, redirect, request, flash
+from app import app
+from app.extensions import db
+from app.form import LoginForm, CategoryForm, PostForm, RegisterForm
+from app.model import Post, User
+from flask_login import login_required, current_user, login_user, logout_user
+
+
+def redirect_back(default='hello', **kwargs):
+    for target in request.args.get('next'), request.referrer:
+        if target:
+            return redirect(target)
+    return redirect(url_for(default, **kwargs))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('/errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def sever_error(e):
+    return render_template('/errors/500.html'), 500
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return render_template('home.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.validate_password(password):
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                flash('用户名或密码出错，请重新输入！')
+                return render_template('login.html', form=form)
+        else:
+            return redirect(url_for('register'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template('home.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        user = User(username=username, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+def sidebar():
+    post_all = Post.query.filter_by(user_id=current_user.id).all()
+    vii = set()
+    for post in post_all:
+        vi = post.category
+        vii.add(vi)
+    categories = [x for x in vii]
+    return categories
+
+
+def sidebars():
+    post_all = Post.query.all()
+    vii = set()
+    for post in post_all:
+        vi = post.category
+        vii.add(vi)
+    categories = [x for x in vii]
+    return categories
+
+
+@app.route('/new_post', methods=['POST', 'GET'])
+@login_required
+def new_post():
+    categories = sidebar()
+    post_form = PostForm()
+    category_form = CategoryForm()
+    if post_form.validate_on_submit():
+        title = post_form.title.data
+        body = post_form.body.data
+        category = category_form.category.data
+        post = Post(title=title, body=body, category=category)
+        db.session.add(post)
+        current_user.post.append(post)
+        db.session.commit()
+        return redirect(url_for('post_list'))
+    return render_template('new_post.html', post_form=post_form,
+                           categories=categories, category_form=category_form)
+
+
+@app.route('/show_post/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get(post_id)
+    posts = Post.query.all()
+    return render_template('show_post.html', post=post, posts=posts)
+
+
+@app.route('/post_list')
+def post_list():
+    posts = Post.query.all()
+    if posts is None:
+        return render_template('post_list.html', posts=None, )
+    categories = sidebars()
+    page = request.args.get('page', type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=3)
+    posts_pagination = pagination.items
+    return render_template('post_list.html', categories=categories, pagination=pagination,
+                           posts_pagination=posts_pagination)
+
+
+@app.route('/my_post')
+@login_required
+def my_post():
+    posts = current_user.post
+    if posts is None:
+        return render_template('post_list.html', posts=None, )
+    categories = sidebar()
+    page = request.args.get('page', type=int)
+    pagination = Post.query.filter_by(
+        user_id=current_user.id).order_by(
+        Post.timestamp.desc()).paginate(page, per_page=3)
+    posts_pagination = pagination.items
+    return render_template(
+            'post_list.html', categories=categories, pagination=pagination, posts_pagination=posts_pagination)
+
+
+@app.route('/post_list_category/<string:category>')
+def post_list_category(category):
+    if current_user.is_authenticated:
+        categories = sidebar()
+        page = request.args.get('page', type=int)
+        pagination = Post.query.filter_by(user_id=current_user.id).filter_by(category=category).paginate(page, per_page=3)
+        posts_pagination = pagination.items
+        return render_template('post_list.html', pagination=pagination,
+                               categories=categories, posts_pagination=posts_pagination)
+    categories = sidebars()
+    page = request.args.get('page', type=int)
+    pagination = Post.query.filter_by(category=category).paginate(page, per_page=3)
+    posts_pagination = pagination.items
+    return render_template('post_list.html', pagination=pagination,
+                           categories=categories, posts_pagination=posts_pagination)
+
+
+@app.route('/delete_post/<int:post_id>', methods=['POST', 'GET'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect_back()
+
+
+@app.route('/edit_post/<int:post_id>', methods=['POST', 'GET'])
+@login_required
+def edit_post(post_id):
+    post_form = PostForm()
+    post = Post.query.get(post_id)
+    posts = Post.query.all()
+    if post_form.validate_on_submit():
+        post.title = post_form.title.data
+        post.body = post_form.body.data
+        db.session.commit()
+        return redirect(url_for('show_post', post_id=post.id))
+    post_form.title.data = post.title
+    post_form.body.data = post.body
+    return render_template('edit_post.html', post=post, posts=posts, post_form=post_form)
